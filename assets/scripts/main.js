b4060dc342e1da30c626f727fc511a0becf1ad5a
joseapintor <joseagarciapintor@gmail.com>
2026-02-05 12:39:32 +0100

    Cleanup main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,16 +6,14 @@ const onScroll = () => {
     const scrollTreshold = 200
     const isOverTreshold = window.scrollY > scrollTreshold
 
-    if (isOverTreshold) {
-        document.documentElement.classList.add(scrollClassName)
-    } else {
-        document.documentElement.classList.remove(scrollClassName)
-    }
+    document.documentElement.classList.toggle(scrollClassName, isOverTreshold)
 }
+
 window.addEventListener('scroll', onScroll, { passive: true })
 
 // Print Button
 const printButton = document.querySelector('.js-print')
+
 printButton.addEventListener('click', () => {
     window.print()
 })
